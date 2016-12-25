ed2409c146323ae0813782c528edd3ab7d51b72e
DapangLiu <rliu68@asu.edu>
2016-12-25 04:11:24 -0700

    signup and logout

--- dashboardapp/views.py
+++ dashboardapp/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+# home
 def home(request):
-    return render(request, 'home.html', {})
+    return redirect(restaurant_home)
+
+# Only the signed in users could logout
+@login_required(login_url='/restaurant/sign-in/')
+def restaurant_home(request):
+    return render(request, 'restaurant/home.html')
