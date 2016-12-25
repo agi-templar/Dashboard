51739e279e2db68e8ae8ce9435111ac006ceca67
DapangLiu <rliu68@asu.edu>
2016-12-25 02:09:23 -0700

    Bootstrap and Dashboard

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'dashboardapp',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
