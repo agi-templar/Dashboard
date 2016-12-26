4f2266e4305b615fbf545124a9c7bbfabd1f1675
DapangLiu <rliu68@asu.edu>
2016-12-26 13:55:33 -0700

    # Page Structure Refac

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from dashboardapp import views
 from django.contrib.auth import views as auth_views
@@ -10,6 +10,8 @@ from django.conf import settings
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
+
+    # Restaurant
     url(r'^restaurant/sign-in/$', auth_views.login,
         {'template_name': 'restaurant/sign_in.html'},
         name = 'restaurant-sign-in'),
@@ -18,5 +20,14 @@ urlpatterns = [
         name = 'restaurant-sign-out'),
     url(r'^restaurant/sign-up', views.restaurant_sign_up,
         name = 'restaurant-sign-up'),
-    url(r'^restaurant/$', views.restaurant_home, name = 'restaurant-home')
+    url(r'^restaurant/$', views.restaurant_home, name = 'restaurant-home'),
+
+    # Four new pages
+    url(r'^restaurant/account/$', views.restaurant_account, name = 'restaurant-account'),
+    url(r'^restaurant/meal/$', views.restaurant_meal, name = 'restaurant-meal'),
+    url(r'^restaurant/order/$', views.restaurant_order, name = 'restaurant-order'),
+    url(r'^restaurant/report/$', views.restaurant_report, name = 'restaurant-report'),
+
+
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
