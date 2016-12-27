f32c78af9eda6d7714307fbda0071efbc5e3d706
DapangLiu <rliu68@asu.edu>
2016-12-27 12:30:42 -0700

    # Cooking -> Ready

--- dashboardapp/views.py
+++ dashboardapp/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from dashboardapp.forms import UserForm, RestaurantForm, UserFormForEdit, MealForm
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
-from dashboardapp.models import Meal
+from dashboardapp.models import Meal, Order
 
 # Create your views here.
 def home(request):
@@ -73,7 +73,16 @@ def restaurant_edit_meal(request, meal_id):
 
 @login_required(login_url='/restaurant/sign-in/')
 def restaurant_order(request):
-    return render(request, 'restaurant/order.html', {})
+    # If the user click the button
+    if request.method == "POST":
+        order = Order.objects.get(id = request.POST["id"], restaurant = request.user.restaurant)
+
+        if order.status == Order.COOKING:
+            order.status = Order.READY
+            order.save()
+
+    orders = Order.objects.filter(restaurant = request.user.restaurant).order_by("-id")
+    return render(request, 'restaurant/order.html', {"orders": orders})
 
 @login_required(login_url='/restaurant/sign-in/')
 def restaurant_report(request):
