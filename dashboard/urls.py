45a6e5495783017b44682e00ea28fa115ed0c02a
DapangLiu <rliu68@asu.edu>
2017-01-05 02:35:44 -0700

    fix

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from dashboardapp import views
 from django.contrib.auth import views as auth_views
 
 from django.conf.urls.static import static
